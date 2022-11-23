d837d932360868c5a214f5cf1a9655b80003ccf7
susu238 <susqub@gmail.com>
2022-11-23 04:16:39 +0200

    watches

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var allProduct = document.querySelectorAll("ul li")
+var content =document.querySelector("#addcontent")
+var btn = document.querySelector("#btn")
+var totalPrice =document.querySelector("#tatalprice")
+var text =document.querySelector("#text")
+var total = 0;
+
+
+allProduct.forEach(function(item){
+    item.onclick = function(){
+        content.innerHTML += text.textContent;
+        total += parseInt(item.getAttribute("price"))
+
+        if (content.innerHTML != ""){
+            btn.style.display="block";
+        }
+    }
+})
+
+btn.onclick=function(){
+    totalPrice.innerHTML = total +"$" ;
+    
+    if(total >= 50){
+        totalPrice.style.background = "beige"
+
+    }
+    else{
+        totalPrice.style.background = "brown "
+    }
+}
+
